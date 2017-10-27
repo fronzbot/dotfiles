e4274c2e314a363d3bef92fc6bdc98cc33f878fb
Kevin Fronczak <kfronczak@gmail.com>
2017-10-27 12:16:14 -0400

    Added colorscheme copying

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 mkdir -r ~/.vim/tmp
 git clone https://github.com/VundleVim/Vundle.vim/git ~/.vim/bundle/Vundle.vim
 bash gen_symlinks.sh
+cp SolarizedDark.colorscheme ~/.kde/share/apps/konsole
 
 echo "Run command: source ~/.bashrc or source ~/.cshrc depending on need."
+echo "Open Konsole, select Settings > Manage Profiles > Edit Profile > Appearance and select Solarized theme"
 echo "Run vim, type :PluginInstall to get plugins. Restart vim"
