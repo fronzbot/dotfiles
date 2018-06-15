f76e69c5bf93f267a18c07555a5ea4213fbed2e2
Kevin Fronczak <kfronczak@gmail.com>
2018-06-15 14:14:55 -0400

    Fix paranthesis highlighting

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ bash gen_symlinks.sh
 cp Molokai.colorscheme ~/.kde/share/apps/konsole/
 cp Molokai.colorscheme ~/.local/share/konsole/
 cp molokai.vim ~/.vim/colors/molokai.vim
+cp monokai.vim ~/.vim/colors/monokai.vim
 
 echo "Run command: source ~/.bashrc or source ~/.cshrc depending on need."
 echo "Open Konsole, select Settings > Manage Profiles > Edit Profile > Appearance and select Molokai theme"
