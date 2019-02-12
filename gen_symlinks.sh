6479c0d41ef4094780d6872a1be2c27b4ab92e4d
Kevin Fronczak <kfronczak@gmail.com>
2019-02-12 10:50:41 -0500

    Update gen_symlinks.sh

--- gen_symlinks.sh
+++ gen_symlinks.sh
@@ -5,8 +5,8 @@
 #-------------------
 # Variables
 #-------------------
-dir=~/Projects/dotfiles
-dir_bak=~/dotfiles_bak
+dir=/root/Projects/dotfiles
+dir_bak=/root/dotfiles_bak
 files=".cshrc .csh_aliases .bashrc .bash_profile .bash_prompt .bash_aliases .vimrc .dircolors .tmux.conf"
 
 
@@ -20,7 +20,7 @@ echo "...done"
 cd $dir
 for file in $files; do
 	echo "Moving old $file and creating new symlink"
-	mv ~/$file $dir_bak
+	mv /root/$file $dir_bak
 	ln -s $dir/$file ~/$file
 done
 
